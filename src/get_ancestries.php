6731af4fb2ce58c8f7447797c3da3414396067c3
glavavlada <glavavlada13@Gmail.com>
2025-08-14 23:40:08 -0700

    PathfinderDashboard_Overview.html was changed to index.html which is the needed name for XAMAPP PHP files with index.html are for the XAMAPP thats working together with PHPMyAdmin which goes into HTDCOCS folder

---
+++ src/get_ancestries.php
@@ -0,0 +1,20 @@
+<?php
+header("Content-Type: application/json");
+
+//connection to MySQL
+$conn = new mysqli("localhost", "root", "", "pathfinder_dashboard");
+
+//check connection
+if ($conn->connect_error) {
+    die(json_encode(["error" => $conn->connect_error]));
+}
+
+// Query table
+$result = $conn->query("SELECT * FROM ancestry"); 
+$data = [];
+while ($row = $result->fetch_assoc()) {
+    $data[] = $row;
+}
+
+echo json_encode($data);
+?>
